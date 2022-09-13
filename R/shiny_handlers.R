36cd12583086a64513477b5fb2008e8c5848e4e4
Russ Hyde <russ@jumpingrivers.com>
2022-09-13 13:23:59 +0100

    feat: convert js 'rgb()' color strings to hex in shiny input-handler

--- R/shiny_handlers.R
+++ R/shiny_handlers.R
@@ -44,5 +44,7 @@ convert_path_data = function(x, session, inputname) {
   rlang::check_installed("purrr")
 
   df = purrr::map_dfr(x, as.data.frame)
+  df$color = parse_js_colors(df$color)
+
   df[c("group", "name", "value", "percentage", "color")]
 }
